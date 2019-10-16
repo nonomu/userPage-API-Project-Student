1987decb6607f54cff28902818bde3172fd17c12
nonomu <noammualmi@gmail.com>
2019-10-16 13:16:45 +0300

    added quote to data with api request

--- APIManager.js
+++ APIManager.js
@@ -6,21 +6,19 @@ class APIManager {
     }
 
     loadData(){
-        //you should make all your API requests here
-        //each request should update the `data` object accordingly
-        // $.ajax({ 
-        //     type: 'GET', 
-        //     url: 'https://api.kanye.rest/',  
-        //     success: (data) => {
-        //         this.data=data
-        //       }
-        // })
         $.ajax({ 
             type: 'GET', 
             url: 'https://randomuser.me/api/?results=7',  
             success: (data) => {
                 this.data.users=data.results.map(r => { return{name : r.name , picture : r.picture.thumbnail}
                 })
+              }
+        })
+        $.ajax({ 
+            type: 'GET', 
+            url: 'https://api.kanye.rest/',  
+            success: (data) => {
+                this.data.quote=data.quote
               }
         })
     }
